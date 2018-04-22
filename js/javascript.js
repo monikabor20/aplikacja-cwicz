fb15cbc4d6f02b8bf6d57bcfc365442a2f07433a
Monika Borawska <monikabor20@wp.pl>
2018-04-22 13:02:42 +0200

    Dodanie funkcji usuwajacej cwiczenia

--- js/javascript.js
+++ js/javascript.js
@@ -1,11 +1,12 @@
-var kliknietoWSekcje ;
+var kliknietoWSekcje;
 document.getElementById('sekcjaNogi').addEventListener("click", function () {
     kliknietoWSekcje = "Nogi";
     showList('myDropdownOne');
+
 });
 
 document.getElementById('sekcjaRece').addEventListener("click", function () {
-   kliknietoWSekcje = "Ręce";
+    kliknietoWSekcje = "Ręce";
     showList('myDropdownTwo');
 });
 
@@ -20,12 +21,12 @@ document.getElementById('sekcjaRozciaganie').addEventListener("click", function 
 });
 
 function showList(id) {
-     const sekcja = document.getElementById(id);
+    const sekcja = document.getElementById(id);
     if (sekcja.style.display === "none") {
         sekcja.style.display = "block";
     } else {
         sekcja.style.display = "none";
-    } 
+    }
 }
 
 function showExercise(target) {
@@ -33,24 +34,22 @@ function showExercise(target) {
     var t = document.createTextNode(`${kliknietoWSekcje} - ${target.innerHTML}`);
     lista.appendChild(t);
     document.getElementById("place").appendChild(lista);
+
+    var deleteButton = document.createElement("button");
+    deleteButton.className = "delete fas fa-check";
+    deleteButton.addEventListener('click', function() {
+        document.getElementById("place").removeChild(lista);
+    })
+    lista.appendChild(deleteButton);
+    return lista;
+
+
+
 }
 
 
 document.addEventListener('click', function (event) {
-    if ( event.target.classList.contains('btn')) {
+    if (event.target.classList.contains('btn')) {
         showExercise(event.target)
     }
 }, false);
-
-
-
-
-
-
-
-
-
-
-
-
-
